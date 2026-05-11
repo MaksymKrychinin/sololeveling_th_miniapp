0e7ae45a9e68c769f270036e8a49e39a1cdf67a2
devdehaddok <devdehaddock@aspin.io>
2026-05-11 12:12:18 +0300

    initial data

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -4,7 +4,7 @@ export { Input } from './components/Input';
 export { ProgressBar } from './components/ProgressBar';
 export { Badge } from './components/Badge';
 export { Modal } from './components/Modal';
-export { Toast, useToast } from './components/Toast';
+export { ToastProvider, useToast } from './components/Toast';
 export { Spinner } from './components/Spinner';
 
 // Types
